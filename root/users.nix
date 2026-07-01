15e5158c6240133506a944428cb254b52fff93f7
zinetin <zinetin2050@proton.me>
2026-07-01 01:46:00 +0100

    working on adding docker

--- root/users.nix
+++ root/users.nix
@@ -1,9 +1,11 @@
 {config, pkgs, ...}:
 
-users.users.zinetin = {
+{
+  users.users.zinetin = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "input" "docker" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
   };
+}
