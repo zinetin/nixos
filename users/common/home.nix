88859d4ac6c04a9d05d500c5af27248752df1d48
zinetin <zinetin2050@proton.me>
2026-07-06 00:38:30 +0100

    Auto Commit from Rebuild Script

---
+++ users/common/home.nix
@@ -0,0 +1,35 @@
+{config, inputs, ...}:
+
+{
+  imports = [
+    ./zen.nix
+    ./iridium.nix
+    ./nv-chad.nix
+    ./packages.nix
+    ./appearance.nix
+  ];
+
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "zinetin";
+      email = "zinetin2050@proton.me";
+    };
+  };
+  
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings = {
+      "github.com" = {
+        HostName = "github.com";
+        User = "git";
+        IdentityFile = "~/.ssh/github";
+        AddKeysToAgent = "yes";
+      };
+    };
+  };
+
+  home.file."Pictures/Screenshots/.keep".text = "";
+ 
+}
--- users/zinetin/home.nix
+++ users/zinetin/home.nix
@@ -5,37 +5,12 @@
   [
 #    ./hypr/hyprland.nix
 #    ./quickshell/quickshell.nix
-    ./iridium.nix
-    ./zen.nix
-    ./appearance.nix
-    ./nv-chad.nix
-    ./packages.nix
+
+    ../common/home.nix
   ];
   home.username = "zinetin";
   home.homeDirectory = "/home/zinetin";
 
-  programs.git = {
-    enable = true;
-    settings.user = {
-      name = "zinetin";
-      email = "zinetin2050@proton.me";
-    };
-  };
-  
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    settings = {
-      "github.com" = {
-        HostName = "github.com";
-        User = "git";
-        IdentityFile = "~/.ssh/github";
-        AddKeysToAgent = "yes";
-      };
-    };
-  };
-
-  home.file."Pictures/Screenshots/.keep".text = "";
-  
+ 
   home.stateVersion = "25.05";
 }
