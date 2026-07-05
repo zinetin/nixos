69d2fed724fad52651af4a69cd4ed2159de1ab3e
zinetin <zinetin2050@proton.me>
2026-07-05 23:52:31 +0100

    Auto Commit from Rebuild Script

--- rebuild.sh
+++ rebuild.sh
@@ -1,14 +1,17 @@
+set -e
+
 if [ -z "$1" ]; then
   echo "Usage: $0 nixflake-name"
   exit 1
 fi
 
-current_dir=$(pwd)
-cd /etc/nixos
+pushd /etc/nixos || exit 1
+
 nix flake update iridium
 git pull
 git add .
 git commit -m "Auto Commit from Rebuild Script"
-sudo nixos-rebuild switch --flake /etc/nixos/#$1
+sudo nixos-rebuild switch --flake "/etc/nixos/#$1"
 git push
-cd current_dir
+
+popd
