8815a7a923db898f0b9345d4f2f7068fca2dab35
zinetin <zinetin2050@proton.me>
2026-07-06 15:17:59 +0100

    Auto Commit from Rebuild Script

--- rebuild.sh
+++ rebuild.sh
@@ -10,7 +10,7 @@ pushd /etc/nixos || exit 1
 nix flake update iridium
 git pull
 git add .
-if [-z "$2" ]; then
+if [ -z "$2" ]; then
   git commit -m "Auto Commit from Rebuild Script"
 else
   git commit -m "$2"
