f02c912b8834ccb04b0fd936f97c9c44e2545e1b
zinetin <zinetin2050@proton.me>
2026-09-15 09:53:45 +0100

    battery-alias

--- users/common/fish.nix
+++ users/common/fish.nix
@@ -6,6 +6,7 @@
     shellAliases = {
       nix-fish = "nix-shell --run fish";
       nsr = "sudo nixos-rebuild switch --flake ~/.mynix/#(hostname)";
+      bat-stat = "cat /sys/class/power_supply/BAT*/{capacity,status}";
     };
   };
 }
