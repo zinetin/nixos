2429362dfde571b489e8011f3ab8e5316e75f23d
zinetin <zinetin2050@proton.me>
2026-09-22 19:46:20 +0100

    Renamed fish aliases

--- users/common/fish.nix
+++ users/common/fish.nix
@@ -5,7 +5,8 @@
     enable = true;
     shellAliases = {
       nix-fish = "nix-shell --run fish";
-      nsr = "sudo nixos-rebuild switch --flake ~/.mynix/#(hostname)";
+      nrs = "sudo nixos-rebuild switch --flake ~/.mynix/#(hostname)";
+      nrb = "sudo nixos-rebuild boot --flake ~/.mynix/#(hostname)";
       bat-stat = "cat /sys/class/power_supply/BAT*/{capacity,status}";
     };
   };
