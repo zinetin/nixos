b1dce2e1cd85fe84955f2091046f184e88e48860
zinetin <zinetin2050@proton.me>
2026-09-25 01:05:09 +0100

    fix-ly?

--- nixos/profiles/desktop/display-manager.nix
+++ nixos/profiles/desktop/display-manager.nix
@@ -16,12 +16,12 @@
 
 
   services.displayManager.ly = {
-    ly.enable = true;
+    enable = true;
 
     settings = {
       animation = "dur_file";
 
-      dur_file_path = "/etc/nixos/root/common/blackhole-smooth-240x67.dur";
+      dur_file_path = ./blackhole-smooth-240x67.dur;
 
       dur_offset_alignment = "center";
 
